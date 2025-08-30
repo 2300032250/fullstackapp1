67290ee10eb3192faeb51e6b93d5f535de3c5563
2300032250 <163723401+2300032250@users.noreply.github.com>
2025-08-30 10:38:43 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://52.55.27.136:9090/springapp1';
+const BASE_URL = 'http://18.209.35.107:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -49,7 +49,7 @@ const BASE_URL = 'http://52.55.27.136:9090/springapp1';
  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://52.55.27.136:9090/springapp1/delete/${id}`);
+      await axios.delete(`http://18.209.35.107:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
